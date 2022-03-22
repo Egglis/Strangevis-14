da3f5d88aca5de2544e6042ff9159c85398a7084
Magne.Stromsnes <magne.stromsnes@student.uib.no>
2022-03-22 15:22:09 +0100

    Texture coordinate is sent as Vec3.

--- shaders/cube-vs.glsl
+++ shaders/cube-vs.glsl
@@ -3,12 +3,12 @@
 uniform vec4 clippingPlaneEquation;
 uniform mat4 modelViewProjectionMatrix;
 
-in vec4 vertexPosition;
+in vec3 vertexPosition;
 out vec3 volumePosition;
 
 void main(void)
 {
-    gl_Position = modelViewProjectionMatrix*vertexPosition;
-    volumePosition = 0.5*(vertexPosition.xyz+vec3(1.0));
-    gl_ClipDistance[0] = dot(clippingPlaneEquation.xyz, vertexPosition.xyz)+clippingPlaneEquation.w;
+    gl_Position = modelViewProjectionMatrix*vec4(vertexPosition,1.0f);
+    volumePosition = 0.5*(vertexPosition+vec3(1.0));
+    gl_ClipDistance[0] = dot(clippingPlaneEquation.xyz, vertexPosition)+clippingPlaneEquation.w;
 }
