5f237a9ed64478dcacf049617e1d5d0b11dacb2e
Egil Bru <43035083+Egglis@users.noreply.github.com>
2022-04-24 14:18:11 +0200

    Graph now also apply transferfunction, fixed a issues with applytransferfunction

--- transferfunction.cpp
+++ transferfunction.cpp
@@ -62,7 +62,7 @@ TransferFunction::applyTransferFunction(const std::vector<GLfloat> cmap)
             auto g = cmap[(t * tfn::size::NUM_CHANNELS) + 1];
             auto b = cmap[(t * tfn::size::NUM_CHANNELS) + 2];
             auto a = getInterpolatedValueBetweenPoints(
-                m_controlPoints.at(i), m_controlPoints.at(i + 1), t);
+                m_controlPoints.at(i), m_controlPoints.at(i + 1), t*(tfn::points::END_POINT.x()/static_cast<float>(255)));
             new_cmap.push_back(r);
             new_cmap.push_back(g);
             new_cmap.push_back(b);
