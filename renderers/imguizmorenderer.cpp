8b9cec98344d30a3bd80ca99a3d20dab35fd471e
Magne.Stromsnes <magne.stromsnes@student.uib.no>
2022-05-11 19:37:05 +0200

    Move imguizmo rendering to separate QOpenGLWidget

---
+++ renderers/imguizmorenderer.cpp
@@ -0,0 +1,50 @@
+#include "imguizmorenderer.h"
+
+// clang-format off
+#include <ImGui.h>
+#include <ImGuizmo.h>
+// clang-format on
+ImguizmoWidget::ImguizmoWidget(std::shared_ptr<ISharedProperties> properties,
+                               CameraProperties& camera, QWidget* parent,
+                               Qt::WindowFlags f)
+    : QOpenGLWidget{parent, f}, m_camera{camera}, m_imGuiReference{nullptr},
+      m_slicingPlaneControls{properties, m_camera}
+
+{
+    setMouseTracking(true);
+    m_refreshTimer = new QTimer();
+    m_refreshTimer->setInterval(1.f/60);
+    connect(m_refreshTimer, &QTimer::timeout, [this](){update();});
+    m_refreshTimer->start();
+}
+void ImguizmoWidget::initializeGL()
+{
+    m_imGuiReference = QtImGui::initialize(this, false);
+}
+
+void ImguizmoWidget::paintGL()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    renderImGuizmo();
+}
+
+void ImguizmoWidget::renderImGuizmo()
+{
+    QtImGui::newFrame(m_imGuiReference);
+
+    ImGuizmo::BeginFrame();
+    ImGuizmo::Enable(true);
+    auto rotationMatrix = m_camera.rotationMatrix();
+    ImGuizmo::ViewManipulate(rotationMatrix.data(), 2.0f * sqrt(3.0f),
+                             ImVec2(0, 0), ImVec2(128, 128), 0);
+    if (m_camera.rotationMatrix() != rotationMatrix)
+    {
+        m_camera.rotateCamera(rotationMatrix);
+        emit updateScene();
+    }
+    m_slicingPlaneControls.paint();
+    ImGui::Render();
+    QtImGui::render(m_imGuiReference);
+}
---
+++ renderers/imguizmorenderer.h
@@ -0,0 +1,34 @@
+#ifndef IMGUIZMORENDERER_H
+#define IMGUIZMORENDERER_H
+
+#include "../properties/cameraproperties.h"
+#include "../properties/sharedproperties.h"
+#include "slicingplanecontrols.h"
+
+#include <QOpenGLWidget>
+#include <QTimer>
+#include <QtImGui.h>
+
+class ImguizmoWidget : public QOpenGLWidget
+{
+    Q_OBJECT
+  public:
+    ImguizmoWidget(std::shared_ptr<ISharedProperties>, CameraProperties& camera,
+                   QWidget* parent = nullptr,
+                   Qt::WindowFlags f = Qt::WindowFlags());
+
+  signals:
+    void updateScene();
+
+  protected:
+    virtual void initializeGL();
+    virtual void paintGL();
+
+  private:
+    void renderImGuizmo();
+    QtImGui::RenderRef m_imGuiReference;
+    CameraProperties& m_camera;
+    SlicingPlaneControls m_slicingPlaneControls;
+    QTimer* m_refreshTimer;
+};
+#endif // IMGUIZMORENDERER_H
